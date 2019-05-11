8bb7428409eec6bf817ae65a852491ee40f27f74
Jacob Hagstedt <jacob.hagstedt@lamneasoftware.se>
2019-05-11 14:14:47 +0200

    Add a put request to be entrypoint to start the scraper

--- webserver/articles.py
+++ webserver/articles.py
@@ -3,18 +3,20 @@ import http
 from os import environ
 
 from aiohttp import web
+from aiohttp.http_exceptions import HttpProcessingError
 
 from repository import Repository
 
 
-repository = Repository(environ['DB_NAME'])
+repository = Repository(environ["DB_NAME"])
 
 
 route = web.RouteTableDef()
 
-logger = logging.getLogger('HTTP-Articles')
+logger = logging.getLogger("HTTP-Articles")
 
-@route.get('/articles')
+
+@route.get("/articles")
 async def get_ranked_articles(request):
     global repository
     logger.info("Client requests ranked articles")
@@ -28,3 +30,14 @@ async def get_ranked_articles(request):
 
     # Return!
     return web.json_response({"ok": True, "data": articles})
+
+
+@route.put("/articles")
+async def save_new_articles(request):
+    global repository
+    logger.info("Client requests to save new articles")
+
+    ## TBI: Call scraper to get new articles!
+
+    # Return!
+    return web.json_response({"ok": True})
