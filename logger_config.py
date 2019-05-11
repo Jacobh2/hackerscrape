eae9f33ed721e316d5f88f6110f061f2547c4da5
Jacob Hagstedt <jacob.hagstedt@lamneasoftware.se>
2019-05-11 12:31:06 +0200

    Update logging settings

--- logger_config.py
+++ logger_config.py
@@ -5,21 +5,13 @@ import yaml
 import os
 
 
-def setup(log_output=None, logging_config_path='/usr/src/app/logging.yaml', only_std_out_level=None):
+def setup(log_output=None, logging_config_path='/usr/src/app/logging.yaml'):
 
     logger = logging.getLogger("App")
 
-    if only_std_out_level is not None:
-        # Setup to only put to stdout
-        logging.basicConfig(
-            level=only_std_out_level,
-            format="%(asctime)s#%(levelname)-7s:%(threadName)-10s:%(name)-10s:%(funcName) -35s:%(lineno) -5d:%(message)s"
-            )
-        return logger
-
     if os.path.exists(logging_config_path):
         with open(logging_config_path) as f:
-            config = yaml.load(f)
+            config = yaml.load(f, Loader=yaml.FullLoader)
         
         # Modify the config if we're given a name!
         if log_output is not None:
