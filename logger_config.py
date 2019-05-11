6138ab58d137e6f77654b53f9815143f3a74ee1b
Jacob Hagstedt <jacob.hagstedt@lamneasoftware.se>
2019-05-11 13:31:30 +0200

    Removed unused logger from code

--- logger_config.py
+++ logger_config.py
@@ -13,10 +13,6 @@ def setup(log_output=None, logging_config_path='/usr/src/app/logging.yaml'):
         with open(logging_config_path) as f:
             config = yaml.load(f, Loader=yaml.FullLoader)
         
-        # Modify the config if we're given a name!
-        if log_output is not None:
-            config['handlers']['mtlog']['name'] = log_output
-
         logging.config.dictConfig(config)
 
     return logger
