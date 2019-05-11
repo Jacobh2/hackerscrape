af72f0fe9912282008cb3b10447d3540d13717da
Marc Jamot <marcjamot@gmail.com>
2019-05-11 14:03:40 +0200

    Add ScrapeManager

---
+++ scrape/__init__.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+from typing import List
+
+import requests
+from bs4 import BeautifulSoup
+
+from objects.article import Article
+
+URL = "https://news.ycombinator.com/newest"
+
+
+class ScrapeManager(object):
+    def scrape(self) -> List[Article]:
+        """Scrapes the first page of Hacker News."""
+        content = self._get(URL)
+        return self._parse(content)
+
+    def _parse(self, content: str) -> List[Article]:
+        soup = BeautifulSoup(content, "html.parser")
+        titles = soup.find_all("a", "storylink")
+        return [
+            Article(
+                header=x.text,
+                author="Anonymous",
+                created=datetime.now(),
+                points=1,
+                link=x["href"],
+                num_comments=0,
+            )
+            for x in titles
+        ]
+
+    def _get(self, url: str) -> str:
+        return requests.get(url).text
